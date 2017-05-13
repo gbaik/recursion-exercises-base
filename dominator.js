4678e8d3dfe825050ab01afa11d2b282ee8632dd
gbaik <gideonbaik@gmail.com>
2017-05-12 23:47:21 -0700

    Add dominator function

--- dominator.js
+++ dominator.js
@@ -2,15 +2,34 @@ const { flattenTreeToArray } = require('./dom-util');
 const _ = require('underscore');
 
 const getElementById = function(root, id) {
-  // Your code here
+  const flattenedArray = flattenTreeToArray(root);
+  for (let i = 0; i < flattenedArray.length; i++) {
+    if (flattenedArray[i].id === id){
+      return flattenedArray[i];
+    };
+  };
 };
 
 const getElementsByClassName = function(root, className) {
-  // Your code here
+  const flattenedArray = flattenTreeToArray(root);
+  const arr = [];
+  for (let i = 0; i < flattenedArray.length; i++){
+    if (flattenedArray[i].className && (flattenedArray[i].className).indexOf(className) !== -1){
+       arr.push(flattenedArray[i]);
+    };
+  };
+  return arr;
 };
 
 const getElementsByTagName = function(root, tagName) {
-  // Your code here
+  const flattenedArray = flattenTreeToArray(root);
+  const arr = [];
+  for (let i = 0; i < flattenedArray.length; i++) {
+    if (flattenedArray[i].tagName === tagName) {
+      arr.push(flattenedArray[i]);
+    };
+  };
+  return arr;
 };
 
 module.exports = {
